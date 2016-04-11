5fceb3b8099c05635240adb5587f338dff4f6659
eescobedo <eescobedo>
2016-04-11 14:18:26 -0600

    First commit and install with configuration npm and gulp

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,10 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+//Route::get('/', 'WelcomeController@index');
+Route::get('/', function (){
+	return view ('pages.home');
+});
 
 Route::get('home', 'HomeController@index');
 
