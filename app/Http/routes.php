d8b1e173bb306f0d8cf342d0fa970f0cf1b2c12c
eescobedo <eescobedo>
2016-04-12 07:51:30 -0600

    Add bootstrap, route and controller for flyers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
 //Route::get('/', 'WelcomeController@index');
 Route::get('/', function (){
@@ -22,3 +12,6 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+
+Route::resource ('flyers', 'FlyersController');
---
+++ resources/views/flyers/create.blade.php
@@ -0,0 +1,6 @@
+@extends ('layout')
+
+@section('content')
+    <h1>Selling Your Home?</h1>
+
+@stop
