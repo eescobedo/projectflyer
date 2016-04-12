e3431b884be50cc3bf603722b5198f63b1c94f70
eescobedo <eescobedo>
2016-04-12 13:55:33 -0600

    PF-3 Add form to use when user create a flyer

--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -3,4 +3,13 @@
 @section('content')
     <h1>Selling Your Home?</h1>
 
+    <hr>
+
+    <form action="/flyers" enctype="multipart/form-data" method="post">
+
+        @include('flyers.form')
+
+    </form>
+
+
 @stop
